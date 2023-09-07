c97443b1ff7d00a2622d76f8ca01a8fe3f8e63a7
“Alba” <albaginesgarcia25@gmail.com>
2023-09-07 11:24:04 +0200

    new variables right

--- js-adakitten/js/main.js
+++ js-adakitten/js/main.js
@@ -2,67 +2,64 @@
 
 const listElement = document.querySelector('.js-list');
 
-/*const kittenOne = `<li class="card">
-<article>*/
 
-const kittenOne = `<li class="card">
-<article>
-  kittenImageOne;
-  kittenNameOne;
-  kittenRaceOne;
-  <h4 class="card_race">Siamés</h4>
-  <p class="card_description">
-            Porte elegante, su patrón de color tan característico y sus ojos
-            de un azul intenso, pero su historia se remonta a Asía al menos
-            hace 500 años, donde tuvo su origen muy posiblemente.
-   </p>
-</article>
-</li>`;
-  const kittenImageOne = `<img
-	class="card_img"
-	src="https://dev.adalab.es/gato-siames.webp"
-	alt="siames-cat"
-  />`;
-  const kittenNameOne = `<h3 class="card_title">Anastacio</h3>`;
-  const kittenRaceOne = `<h4 class="card_race">Siamés</h4>`;
-  const kittenDescOne = `<p class="card_description">
+const kittenImageOne = `https://dev.adalab.es/gato-siames.webp`;
+const kittenNameOne = `Anastacio`;
+const kittenRaceOne = `Siamés`;
+const kittenDescOne = `
 			Porte elegante, su patrón de color tan característico y sus ojos
 			de un azul intenso, pero su historia se remonta a Asía al menos
-			hace 500 años, donde tuvo su origen muy posiblemente.
-   </p>`;
+			hace 500 años, donde tuvo su origen muy posiblemente.`;
 
+const kittenOne = `<li class="card">
+	<article>
+	<img
+	class="card_img"
+    src="${kittenImageOne}"
+	alt="siames-cat"
+	/>
+	<h3 class="card_title">${kittenNameOne}</h3>
+	<h4 class="card_race">${kittenRaceOne}</h4>
+	<p class="card_description">${kittenDescOne}</p>
+	</article>
+	</li>`;
 
-/*</article>
-</li>`;*/
+const kittenImageTwo = `https://dev.adalab.es/sphynx-gato.webp`;
+const kittenNameTwo = `Fiona`;
+const kittenRaceTwo = `Sphynx`;
+const kittenDescTwo = 
+` Produce fascinación y curiosidad.  Exótico, raro, bello, extraño… 
+hasta con pinta de alienígena han llegado a definir a esta raza 
+gatuna que se caracteriza por la «ausencia» de pelo.`;
 
 const kittenTwo = `<li class="card"> 
 	<img 
 		class="card_img"
-		src="https://dev.adalab.es/sphynx-gato.webp"
+		src="${kittenImageTwo}"
 		alt="sphynx-cat"
 	/> 
-	<h3 class="card_title">Fiona</h3> 
-	<h4 class="card_race">Sphynx</h4> 
-	<p class="card_description"> 
-			Produce fascinación y curiosidad. Exótico, raro, bello, extraño… 
-			hasta con pinta de alienígena han llegado a definir a esta raza 
-			gatuna que se caracteriza por la «ausencia» de pelo.
-	</p> 
-</li>`; 
+	<h3 class="card_title">${kittenNameTwo}</h3> 
+	<h4 class="card_race">${kittenRaceTwo}</h4> 
+	<p class="card_description"> ${kittenDescTwo}</p> 
+    </li>`; 
+
+const kittenImageThree = `https://dev.adalab.es/maine-coon-cat.webp`;
+const kittenNameThree = `Cielo`;
+const kittenRaceThree= `Maine Coon`;
+const kittenDescThree= 
+`Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
+bella mirada se ha convertido en una de sus señas de identidad.
+Sus ojos son grandes y las orejas resultan largas y en punta.`;
 
 const kittenThree = `<li class="card">
 	<img
 		class="card_img"
-		src="https://dev.adalab.es/maine-coon-cat.webp"
+		src="${kittenImageThree}"
 		alt="maine-coon-cat"
 	/>
-	<h3 class="card_title">Cielo</h3>
-	<h4 class="card_race">Maine Coon</h4>
-	<p class="card_description">
-			Tienen la cabeza cuadrada y los ojos simétricos, por lo que su
-			bella mirada se ha convertido en una de sus señas de identidad.
-			Sus ojos son grandes y las orejas resultan largas y en punta.
-	</p>
+	<h3 class="card_title">${kittenNameThree}</h3>
+	<h4 class="card_race">${kittenRaceThree}</h4>
+	<p class="card_description">${kittenDescThree}</p>
 	</li>`; 
 
 
